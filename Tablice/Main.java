732b5ae206cf6c09780bfec5a4b5448b1d971d44
Azi <natalianowacka2002@gmail.com>
2025-02-25 20:25:56 +0100

    tablice

---
+++ Tablice/Main.java
@@ -0,0 +1,15 @@
+package Tablice;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[] tablica = {1, 20, 30, 50, 60};
+
+        System.out.println(tablica[0] + tablica[1] + tablica[2] + tablica[3] + tablica[4]);
+    }
+
+}
+
+
+
